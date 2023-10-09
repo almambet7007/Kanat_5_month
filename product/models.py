561eca3b2fde2675d518aa885f00b816f3860eb8
almambet7007 <abdyldaevalmambet14@gmail.com>
2023-10-09 18:47:48 +0600

    hw4

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.core import validators
 
@@ -15,17 +16,31 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    price = models.CharField(max_length=255)
+    price = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    tag = models.ManyToManyField('Tag')
     def __str__(self):
         return self.title
 
+    def clean(self):
+        # Переопределяем метод clean для валидации тэгов
+        super().clean()
+        for tag in self.tags.all():
+            if not Tag.objects.filter(name=tag.name).exists():
+                raise ValidationError(f"Тэг '{tag.name}' не существует в базе данных. Укажите существующие тэги.")
 
 class Review(models.Model):
     text = models.CharField(max_length=255)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    starts = models.IntegerField(validators=[validators.MinValueValidator(1), validators.MaxValueValidator(5)])
+    stars = models.IntegerField(validators=[validators.MinValueValidator(1), validators.MaxValueValidator(5)])
 
     def __str__(self):
         return self.text
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
--- product/serializers.py
+++ product/serializers.py
@@ -3,18 +3,29 @@ from .models import Product, Review, Category
 
 
 class ProductSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255, required=True)
+    description = serializers.CharField(max_length=255, required=True)
+    price = serializers.IntegerField(min_value=1, required=True)
+    category = serializers.IntegerField(required=True)
+
     class Meta:
         model = Product
         fields = '__all__'
 
 
 class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255, required=True)
+
     class Meta:
         model = Category
         fields = '__all__'
 
 
 class ReviewSerializer(serializers.Serializer):
+    text = serializers.CharField(max_length=255, required=False)
+    product = serializers.IntegerField(required=True)
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
     class Meta:
         model = Review
         fields = '__all__'
